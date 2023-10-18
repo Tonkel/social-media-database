7838b5a9ab8dccacb20f46b8e9aff50680ae23c4
Tonkel <w.tucker.cac@gmail.com>
2023-10-18 18:25:37 -0400

    creates basic layout, creates one schema

---
+++ models/reaction.js
@@ -0,0 +1,33 @@
+const { Schema, Types } = require("mongoose");
+
+const reactionSchema = new Schema(
+  {
+    reaction_id: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+reactionSchema.virtual("createdAt").get(function () {
+  return this.createdAt.toLocaleString();
+});
